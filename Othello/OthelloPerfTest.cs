e90a2b45665f5f4f7ea7e718dc9a559c2fba9ae9
Tetsuaki Otsuki <chimney37@hotmail.com>
2020-01-02 10:12:15 +0900

    make perf test managable.

--- Othello/OthelloPerfTest.cs
+++ Othello/OthelloPerfTest.cs
@@ -59,7 +59,7 @@ namespace Othello
             var target = new OthelloGame(oPlayerA, oPlayerB, oPlayerA);
             OthelloGame.GameDisableLog();
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {     
                 target.GameMakeMove(5, 4, oPlayerA);
                 target.GameMakeMove(5, 3, oPlayerB);
